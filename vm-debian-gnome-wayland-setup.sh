e09faea1629abaab7d6fad110d611f18cf5240bc
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-06 17:24:52 +0530

    Create vm-debian-gnome-wayland-setup.sh

---
+++ vm-debian-gnome-wayland-setup.sh
@@ -0,0 +1,5 @@
+# disable automatic screenlock
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+# disable automatic screen blank
+gsettings set org.gnome.desktop.session idle-delay 0
