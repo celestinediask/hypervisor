9c8462d950825407980e1c311a2e6079458e84b2
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-06 17:52:59 +0530

    Create arch-hv-setup.sh

---
+++ arch-hv-setup.sh
@@ -0,0 +1,6 @@
+# hypervisor setup on archlinux
+sudo pacman -S virt-manager qemu libvirt dnsmasq
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+sudo virsh net-start default
+sudo usermod -aG libvirt $(whoami)
